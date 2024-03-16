98854a9e26203f2b4881bc251ad11833d0d0eff1
Moabe de jesus souza <moabe.2009@hotmail.com>
2024-03-16 20:40:29 -0300

    criação das interfaces genericas

---
+++ Ecom.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task <IReadOnlyList<T>> GetAllAsync();
+        IEnumerable<T> GetAll();
+
+        Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includes);
+        IEnumerable<T> GetAll(params Expression<Func<T, object>>[] includes);
+   
+
+       
+
+        Task<T> GetByIdAsync(T id);
+        Task AddAsync (T entity);
+        Task UpdateAsync(T id,T entity);
+        Task DeleteAsync(T id);
+    }
+}
