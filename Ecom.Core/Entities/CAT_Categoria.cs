3d0c47856fd70e252f8667c39fc02101a7d5ed94
Moabe de jesus souza <moabe.2009@hotmail.com>
2024-03-16 20:39:44 -0300

    criação das models

---
+++ Ecom.Core/Entities/CAT_Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Core.Entities
+{
+    public class CAT_Categoria : BaseEntity<int>
+    {
+        public string cat_nm_nome {get; set;}
+        public string cat_ds_descricao { get; set; }
+
+        public virtual ICollection <PRD_Produto> Produto { get; set; }
+
+    }
+}
---
+++ Ecom.Core/Entities/PRD_Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Core.Entities
+{
+    public class PRD_Produto:BaseEntity<int>
+    {
+        public string prd_nm_nome { get; set; }
+        public string prd_ds_descricao { get; set; }
+        public decimal prd_vl_valor { get; set; }
+        public int prd_id_categoria { get; set; }
+
+        public virtual CAT_Categoria Categoria { get; set; }
+    }
+}
