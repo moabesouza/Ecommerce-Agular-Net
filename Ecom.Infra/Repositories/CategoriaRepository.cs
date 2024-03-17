0c08010354185f0cc0d33d36d1d0bb23cbad70f3
Moabe Souza <moabesouza6@gmail.com>
2024-03-16 22:08:15 -0300

    criação do repository de categoria e produto

---
+++ Ecom.Infra/Repositories/CategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using Ecom.Core.Entities;
+using Ecom.Core.Interfaces;
+using Ecom.Infra.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infra.Repositories
+{
+    public class CategoriaRepository : GenericRepository<CAT_Categoria>, ICategoriaRepository
+    {
+        public CategoriaRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
